9c6e8bc81f23527fdb9ff56f44a35aabc375f748
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2024-02-13 11:41:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // your code here
+const form= document.getElementById("form");
+const url= document.getElementById("url");
+form.addEventListener("submit", (e)=>{
+	e.preventDefault();
+	const name= form.name.value;
+	const year= form.year.value;
+
+	if(name && year){
+		url.innerHTML="";
+		url.innerHTML= `https://localhost:8080/?name=${name}&year=${year}`
+	}
+	else if(name){
+		url.innerHTML="";
+		url.innerHTML= `https://localhost:8080/?name=${name}`
+	}
+	else if(year){
+		url.innerHTML="";
+		url.innerHTML= `https://localhost:8080/?year=${year}`
+	}
+	console.log('submitted',name,year)
+})
